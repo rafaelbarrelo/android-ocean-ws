f62c5dec2385e10f25a37fd986c70ec6caa17b5f
Rafael Barrelo <rafaelbarrelo@gmail.com>
2016-08-21 18:10:44 -0300

    jdk8

--- app/src/main/java/com/example/samsung/githubapp/MainActivity.java
+++ app/src/main/java/com/example/samsung/githubapp/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.samsung.githubapp;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
